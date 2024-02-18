2476b369dd4877ec17578299c4e8bab33993814c
krloswiguel <71908167+krloswiguel@users.noreply.github.com>
2024-02-17 20:34:53 -0500

    Update srt_stream.sh
    srt video con correcion de desconexion de redes

--- srt_stream.sh
+++ srt_stream.sh
@@ -2,13 +2,22 @@
 
 URL_DEL_STREAM="srt://DIRECCION_DEL_SERVIDOR:PUERTO/STREAM"
 
+# Función para verificar la conectividad a Internet
+verificar_conexion() {
+    ping -c 1 google.com > /dev/null 2>&1
+    return $?
+}
+
 while true; do
     # Verificar la conectividad a Internet
-    if ping -c 1 google.com > /dev/null 2>&1; then
-        # Reproducir el stream usando ffplay en modo pantalla completa y con bucle continuo
-        ffplay -hide_banner -fs -i "$URL_DEL_STREAM" -loop 0
+    verificar_conexion
+    CONEXION=$?
+
+    if [ $CONEXION -eq 0 ]; then
+        # Si hay conexión a Internet, intentar reproducir el flujo SRT
+        ffmpeg -hide_banner -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -i "$URL_DEL_STREAM" -vf "fullscreen" -nostats -loglevel 0 -nostdin -autoexit -f null -
     else
-        # Si la conexión a Internet se pierde, espera unos segundos y vuelve a intentar
+        # Si no hay conexión a Internet, esperar y volver a intentar
         echo "Conexión a Internet perdida. Reintentando en 10 segundos..."
         sleep 10
     fi
